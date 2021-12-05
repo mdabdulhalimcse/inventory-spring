c55d881284ed5af5a3e9707ddebd8fa914cc8a29
Abdul Halim <45326906+mdabdulhalimcse@users.noreply.github.com>
2021-12-05 23:26:54 +0600

    Update ModelsServiceImpl.java

--- src/main/java/com/mmtvbd/service/serviceimpl/ModelsServiceImpl.java
+++ src/main/java/com/mmtvbd/service/serviceimpl/ModelsServiceImpl.java
@@ -9,6 +9,7 @@ import com.mmtvbd.service.ModelsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -18,25 +19,17 @@ public class ModelsServiceImpl implements ModelsService {
     private ModelsRepo modelsRepo;
     @Autowired
     private BrandRepo brandRepo;
+    @Autowired
+    EntityManager entityManager;
 
     @Override
-    public List<Models> modelsUpdate(ObjectNode objectNode) {
-        String brandName = objectNode.get("brandName").asText();
-        List<Brand> brands = this.brandRepo.findAllById(Long.parseLong(brandName));
-        String getBrandName= brands.get(0).getBrandName();
-        List<Models> models1 = this.modelsRepo.findAll();
-        boolean result = true;
-        for (Models s: models1) {
-            if (getBrandName.equals(s.getBrand().getBrandName()) && s.getModelName().equals(objectNode.get("modelName").asText())) {
-                System.out.println("Already exists! Please try again...");
-                result = false;
-                break;
-            } else {
-                result = true;
-            }
-        }
-
-        if (result == true){
+    public String modelsUpdate(ObjectNode objectNode) {
+        String brandI = objectNode.get("brandName").asText();
+        String modelName = objectNode.get("modelName").asText();
+        Long bId = Long.parseLong(brandI);
+        List<String> modelItems = entityManager.createNativeQuery(" SELECT model_name FROM inventory_db.models where model_name = '"+modelName+"' and brand_id= "+bId+"").getResultList();
+        int modelCheck = modelItems.size();
+        if (modelCheck == 0){
             String brandId = objectNode.get("brandName").asText();
             Models models = modelsRepo.getOne(Long.valueOf(objectNode.get("id").asText()));
             Timestamp timestamp = new Timestamp(System.currentTimeMillis());
@@ -48,28 +41,20 @@ public class ModelsServiceImpl implements ModelsService {
 
             models.setBrand(brand);
             modelsRepo.save(models);
+
+            return "success";
         }
-        return null;
+        return "dublicate";
     }
 
     @Override
-    public List<Models> modelsSave(ObjectNode objectNode, Models models) {
-        String brandName = objectNode.get("brandName").asText();
-        List<Brand> brands = this.brandRepo.findAllById(Long.parseLong(brandName));
-        String getBrandName= brands.get(0).getBrandName();
-        List<Models> models1 = this.modelsRepo.findAll();
-        boolean result = true;
-        for (Models s: models1) {
-            if (getBrandName.equals(s.getBrand().getBrandName()) && s.getModelName().equals(objectNode.get("modelName").asText())) {
-                System.out.println("Already exists! Please try again...");
-                result = false;
-                break;
-            } else {
-                result = true;
-            }
-        }
-
-        if (result == true){
+    public String modelsSave(ObjectNode objectNode, Models models) {
+        String brandI = objectNode.get("brandName").asText();
+        String modelN = objectNode.get("modelName").asText();
+        Long bId = Long.parseLong(brandI);
+        List<String> modelItems = entityManager.createNativeQuery(" SELECT model_name FROM inventory_db.models where model_name = '"+modelN+"' and brand_id= "+bId+"").getResultList();
+        int modelCheck = modelItems.size();
+        if (modelCheck == 0){
             String brandId = objectNode.get("brandName").asText();
             String modelName = objectNode.get("modelName").asText();
             Timestamp timestamp = new Timestamp(System.currentTimeMillis());
@@ -79,7 +64,8 @@ public class ModelsServiceImpl implements ModelsService {
             brand.setId(Long.parseLong(brandId));
             models.setBrand(brand);
             modelsRepo.save(models);
+            return "success";
         }
-        return null;
+        return "dublicate";
     }
 }
